c6a8f47e3b12b36ca6599f244d1a2dd7d511b275
thippani-saikumar <tsayi1998@gmail.com>
2022-11-30 18:40:36 +0530

    home link added

--- frontendp/frontendp/urls.py
+++ frontendp/frontendp/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('users.urls')),
+    path('users/',include('users.urls')),
     path('',include('post.urls')),
     path('',include('ourcompany.urls')),
     
